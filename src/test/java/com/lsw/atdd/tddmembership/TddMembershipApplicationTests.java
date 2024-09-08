3fcc58c15a22d2a4ddfe1b743343c46b2b9a88f5
seungwon <swlee173@gmail.com>
2024-09-08 21:35:31 +0900

    init

---
+++ src/test/java/com/lsw/atdd/tddmembership/TddMembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsw.atdd.tddmembership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddMembershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
