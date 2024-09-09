190ae96d6f6fe7ba483e906f877b2a7450955a0a
seungwon <swlee173@gmail.com>
2024-09-09 16:16:30 +0900

    이름 수정

--- src/main/java/com/lsw/atdd/tddmembership/membership/MembershipType.java
+++ src/main/java/com/lsw/atdd/tddmembership/membership/MembershipType.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum MemebershipType {
+public enum MembershipType {
     NAVER("네이버")
     , LINE("라인")
     , KAKAO("카카오");
