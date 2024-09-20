3eef4836511dced0276d8a07fca88b895fc71fa3
seungwon <swlee173@gmail.com>
2024-09-20 15:32:19 +0900

    멤버십 정보 조회 테스트 코드 작성

--- src/main/java/com/lsw/atdd/tddmembership/membership/MembershipRepository.java
+++ src/main/java/com/lsw/atdd/tddmembership/membership/MembershipRepository.java
@@ -2,7 +2,10 @@ package com.lsw.atdd.tddmembership.membership;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
     Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
 
+    List<Membership> findAllByUserId(String userId);
 }
